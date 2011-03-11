a7d4901f1bdaa365df50a6375365278363350004
q3rally <q3rally@b95fa9d2-619b-4617-8743-2613d58f007a>
2011-03-11 21:32:53 +0000

    Changed build Number

--- engine/code/qcommon/q_shared.h
+++ engine/code/qcommon/q_shared.h
@@ -44,7 +44,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 #ifdef _MSC_VER
-  #define PRODUCT_VERSION "0.1 build 18"
+  #define PRODUCT_VERSION "0.1 build 25"
 #endif
 
 #define Q3_VERSION PRODUCT_NAME " " PRODUCT_VERSION
