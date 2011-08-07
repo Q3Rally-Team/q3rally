ec381a5c9f15ec40dc23eae8b6edb30b28cf9f76
zturtleman <zturtleman@b95fa9d2-619b-4617-8743-2613d58f007a>
2011-08-07 04:00:27 +0000

    Run scripts now run build command for you. Run scripts now create links to game logic instead of telling user to copy them.

--- run.sh
+++ run.sh
@@ -5,24 +5,21 @@ PLATFORM=`uname|sed -e s/_.*//|tr '[:upper:]' '[:lower:]'|sed -e 's/\//_/g'`
 ARCH=`uname -m | sed -e s/i.86/i386/`
 BUILD=release
 
-BIN=q3rally.$ARCH
-
-if [ ! -f $BIN ]; then
 BIN=engine/build/$BUILD-$PLATFORM-$ARCH/q3rally.$ARCH
-fi
 
 if [ ! -f $BIN ]; then
-	echo "Game binary '$BIN' not found!"
-	echo "Run 'make -C engine' to build it."
-	exit 1
+	echo "Game binary '$BIN' not found, building it..."
+	make -C engine BUILD_GAME_SO=0 BUILD_SERVER=0
 fi
 
+# Create links to game logic qvms
 if [ ! -f baseq3r/vm/ui.qvm ]; then
-	echo "QVMs not found!"
-	echo "Run 'cp engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/*.qvm baseq3r/vm/'"
-	exit 1
+	DIR=`pwd`
+	ln -st "$DIR/baseq3r/vm/" "$DIR/engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/cgame.qvm"
+	ln -st "$DIR/baseq3r/vm/" "$DIR/engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/qagame.qvm"
+	ln -st "$DIR/baseq3r/vm/" "$DIR/engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/ui.qvm"
 fi
 
 # Run the game
-./$BIN +set fs_basepath "." +set vm_game 0 +set vm_cgame 0 +set vm_ui 0 $@
+./$BIN +set fs_basepath "." +set vm_game 2 +set vm_cgame 2 +set vm_ui 2 $@
 
