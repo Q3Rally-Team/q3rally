3db14f4f6574e3e9d1a6c9e3881fd5ac629ad35d
q3rally <q3rally@b95fa9d2-619b-4617-8743-2613d58f007a>
2016-05-18 07:57:17 +0000

    bots should use all weapons now

--- engine/builds/release-linux-x86_64/tools/rcc/dagcheck.c
+++ engine/builds/release-linux-x86_64/tools/rcc/dagcheck.c
@@ -6,7 +6,7 @@ typedef Node NODEPTR_TYPE;
 #define STATE_LABEL(p)  ((p)->x.state)
 #define PANIC	   error
 /*
-generated at Tue May 17 23:20:08 2016
+generated at Wed May 18 06:30:06 2016
 by lburg.c - faked rcsid
 */
 static void _kids(NODEPTR_TYPE, int, NODEPTR_TYPE[]);
