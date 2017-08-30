7fb48b29ca57b68ec3e29232b56ec6fde302412d
zturtleman <zturtleman@b95fa9d2-619b-4617-8743-2613d58f007a>
2017-08-30 07:09:41 +0000

    ioquake3 resync to revision 3347 from 3331.
    Update arch in misc/setup for ioq3 changing from i386 to x86
    OpenGL2: Use extension functions with OpenGL versions before 3.0.
    OpenGL2: Force VAO usage on OpenGL 3.0+
    Add spawnflags to QUAKED for trigger_multiple
    Check for all command separators in callTeamVote
    Remove newlines from chat messages in Game VM
    Fix compiling Cmd_CallTeamVote_f
    Only allow safe protocols for cURL downloads
    Fix crash when pmove_msec is 0
    Combine mouse movement events in event queue
    Improve keys using international key layouts
    Improve client input responsiveness
    Removing input functions from sys_local.h for last commit
    Make input events use earliest possible time
    Make bots use crusher on other q3tourney6 maps
    Make bots only use q3tourney6 crusher to kill their enemy

--- engine/code/game/g_cmds.c
+++ engine/code/game/g_cmds.c
@@ -1152,6 +1152,16 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 	}
 }
 
+static void SanitizeChatText( char *text ) {
+	int i;
+
+	for ( i = 0; text[i]; i++ ) {
+		if ( text[i] == '\n' || text[i] == '\r' ) {
+			text[i] = ' ';
+		}
+	}
+}
+
 
 /*
 ==================
@@ -1173,6 +1183,8 @@ static void Cmd_Say_f( gentity_t *ent, int mode, qboolean arg0 ) {
 	{
 		p = ConcatArgs( 1 );
 	}
+
+	SanitizeChatText( p );
 
 	G_Say( ent, NULL, mode, p );
 }
@@ -1205,6 +1217,8 @@ static void Cmd_Tell_f( gentity_t *ent ) {
 	}
 
 	p = ConcatArgs( 2 );
+
+	SanitizeChatText( p );
 
 	G_LogPrintf( "tell: %s to %s: %s\n", ent->client->pers.netname, target->client->pers.netname, p );
 	G_Say( ent, target, SAY_TELL, p );
@@ -1304,6 +1318,8 @@ static void Cmd_Voice_f( gentity_t *ent, int mode, qboolean arg0, qboolean voice
 		p = ConcatArgs( 1 );
 	}
 
+	SanitizeChatText( p );
+
 	G_Voice( ent, NULL, mode, p, voiceonly );
 }
 
@@ -1335,6 +1351,8 @@ static void Cmd_VoiceTell_f( gentity_t *ent, qboolean voiceonly ) {
 	}
 
 	id = ConcatArgs( 2 );
+
+	SanitizeChatText( id );
 
 	G_LogPrintf( "vtell: %s to %s: %s\n", ent->client->pers.netname, target->client->pers.netname, id );
 	G_Voice( ent, target, SAY_TELL, id, voiceonly );
@@ -1685,6 +1703,7 @@ Cmd_CallTeamVote_f
 ==================
 */
 void Cmd_CallTeamVote_f( gentity_t *ent ) {
+	char*	c;
 	int		i, team, cs_offset;
 	char	arg1[MAX_STRING_TOKENS];
 	char	arg2[MAX_STRING_TOKENS];
@@ -1724,9 +1743,16 @@ void Cmd_CallTeamVote_f( gentity_t *ent ) {
 		trap_Argv( i, &arg2[strlen(arg2)], sizeof( arg2 ) - strlen(arg2) );
 	}
 
-	if( strchr( arg1, ';' ) || strchr( arg2, ';' ) ) {
-		trap_SendServerCommand( ent-g_entities, "print \"Invalid vote string.\n\"" );
-		return;
+	// check for command separators in arg2
+	for( c = arg2; *c; ++c) {
+		switch(*c) {
+			case '\n':
+			case '\r':
+			case ';':
+				trap_SendServerCommand( ent-g_entities, "print \"Invalid vote string.\n\"" );
+				return;
+			break;
+		}
 	}
 
 	if ( !Q_stricmp( arg1, "leader" ) ) {
--- engine/code/qcommon/qcommon.h
+++ engine/code/qcommon/qcommon.h
@@ -1064,6 +1064,14 @@ int SV_SendQueuedPackets(void);
 qboolean UI_GameCommand( void );
 qboolean UI_usesUniqueCDKey(void);
 
+//
+// input interface
+//
+void IN_Init( void *windowData );
+void IN_Frame( void );
+void IN_Shutdown( void );
+void IN_Restart( void );
+
 /*
 ==============================================================
 
