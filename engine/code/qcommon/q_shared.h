456486753943880d0be252c4e2e75b58bb03586b
q3rally <q3rally@b95fa9d2-619b-4617-8743-2613d58f007a>
2020-07-08 18:16:59 +0000

    changed version and year numbers

--- engine/code/qcommon/q_shared.h
+++ engine/code/qcommon/q_shared.h
@@ -67,7 +67,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define BASETA				"missionpack"
 
 #ifndef PRODUCT_VERSION
-#define PRODUCT_VERSION "v0.0.2.5_r265"
+#define PRODUCT_VERSION "v0.0.2.8_r274"
 #endif
 
 
