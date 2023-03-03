bf32458d4aa11a223b63eacef9cd6b387aa7946f
Zack Middleton <zack@cloemail.com>
2023-03-02 21:49:20 -0600

    Fix run.sh for moving to git
    "baseq3r/vm" directory was lost in the move to git; subversion allows
    empty directories but git does not. Make run.sh create the directory
    so symlinks can be made. Otherwise run.sh fails to run the game.

--- run.sh
+++ run.sh
@@ -9,12 +9,13 @@ BIN=engine/build/$BUILD-$PLATFORM-$ARCH/q3rally.$ARCH
 
 if [ ! -f $BIN ]; then
 	echo "Game binary '$BIN' not found, building it..."
-	make -C engine BUILD_GAME_SO=0 BUILD_SERVER=0
+	make -C engine $BUILD BUILD_GAME_SO=0 BUILD_SERVER=0
 fi
 
 # Create links to game logic qvms
 if [ ! -f baseq3r/vm/ui.qvm ]; then
 	DIR=`pwd`
+	mkdir -p baseq3r/vm
 	ln -st "$DIR/baseq3r/vm/" "$DIR/engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/cgame.qvm"
 	ln -st "$DIR/baseq3r/vm/" "$DIR/engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/qagame.qvm"
 	ln -st "$DIR/baseq3r/vm/" "$DIR/engine/build/$BUILD-$PLATFORM-$ARCH/baseq3r/vm/ui.qvm"
