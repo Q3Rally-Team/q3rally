8b1de85aa04d3dad643b82421719ff6b4229bf33
insellium <anrealskiydubz@gmail.com>
2023-12-11 06:40:33 +0500

    GrenadeL alt fire fix

--- engine/code/game/g_missile.c
+++ engine/code/game/g_missile.c
@@ -676,7 +676,7 @@ void G_RunMissile( gentity_t *ent ) {
 	else {
 		VectorCopy( tr.endpos, ent->r.currentOrigin );
 	}
-	if ( ent->s.weapon == WP_ROCKET_LAUNCHER ) {
+	if ( ent->s.weapon == WP_ROCKET_LAUNCHER || !strcmp(ent->classname,"fire_cluster_grenade") ) {
 	Missile_Smooth_H(ent,origin,&tr);
 	}
 	trap_LinkEntity( ent );
